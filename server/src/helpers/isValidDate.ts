968b5775899819f1dd7f40b5cfdcf93b185b7704
Nidsonr1 <nidsoncampos@hotmail.com>
2023-12-18 20:25:00 -0300

    Feat: Tratar erro para caso o data do fim da consulta tenha um dia diferente do dia da data inicial

--- server/src/helpers/isValidDate.ts
+++ server/src/helpers/isValidDate.ts
@@ -1,4 +1,7 @@
 export function isValidDate(startDate: Date, endDate: Date, currentDate: Date) {
+	const inititalDay = startDate.getDate();
+	const endDay = endDate.getDate();
+
 	if (startDate < currentDate) {
 		return 'A data de início deve estar no futuro.';
 	}
@@ -9,5 +12,9 @@ export function isValidDate(startDate: Date, endDate: Date, currentDate: Date) {
 
 	if (endDate <= startDate) {
 		return 'A data de término deve ser posterior à data de início.';
+	}
+
+	if (inititalDay != endDay) {
+		return 'A data de início e de término tem que está no mesmo dia.';
 	}
 }
