83f861c219f960c96fecea3835ae1b85e5fa8aa8
Nidsonr1 <nidsoncampos@hotmail.com>
2024-06-22 14:36:12 -0300

    Ajustando porta para rodar a aplicação

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,6 @@
 import { app } from './app';
 import { env } from './env';
 
-app.listen(3333, () => {
-	console.log(`🚀 Server is running in port ${3333}`);
+app.listen(process.env.PORT || 3333, () => {
+	console.log('🚀 Server is running!');
 });
